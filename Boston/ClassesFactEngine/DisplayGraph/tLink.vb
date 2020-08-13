1ff1fc2f67500c9bf1c7e0bd5de5152fffa48999
Victor Morgante <victormorgante@viev.com>
2020-08-13 16:46:59 +1000

    - Added new tLink and tNode in FactEngine.DisplayGraph namespace, for displaying a graph of results.

---
+++ Boston/ClassesFactEngine/DisplayGraph/tLink.vb
@@ -0,0 +1,8 @@
+﻿Namespace FactEngine.DisplayGraph
+    Public Class Link
+
+        Public Link As MindFusion.Diagramming.DiagramLink = Nothing
+
+    End Class
+
+End Namespace
---
+++ Boston/ClassesFactEngine/DisplayGraph/tNode.vb
@@ -0,0 +1,26 @@
+﻿Namespace FactEngine.DisplayGraph
+    Public Class Node
+
+        Public Table As RDS.Table
+        Public Column As New List(Of RDS.Column)
+
+        Public Shape As MindFusion.Diagramming.Shape = Nothing
+
+        Public Edge As List(Of FactEngine.DisplayGraph.Link)
+
+        Public Sub New(ByVal arTable As RDS.Table,
+                       ByVal aarColumn As List(Of RDS.Column),
+                       ByVal aarLink As List(Of FactEngine.DisplayGraph.Link))
+
+            Me.Table = arTable
+            Me.Column = aarColumn
+
+        End Sub
+
+        Public Sub DisplayAndAssociate()
+
+        End Sub
+
+    End Class
+
+End Namespace
