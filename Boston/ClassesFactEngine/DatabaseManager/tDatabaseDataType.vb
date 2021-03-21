e5a3f00ab263785019bb9126dc643ea191de8cdb
victormorgante <victormorgante@viev.com>
2021-03-21 12:10:01 +1000

    - Adding new files to the FactEngine suite of classes.

---
+++ Boston/ClassesFactEngine/DatabaseManager/tDatabaseDataType.vb
@@ -0,0 +1,34 @@
+﻿Public Class DatabaseDataType
+
+    Private _Database As pcenumDatabaseType
+    Public Property Database As pcenumDatabaseType
+        Get
+            Return Me._Database
+        End Get
+        Set(value As pcenumDatabaseType)
+            Me._Database = value
+        End Set
+    End Property
+
+
+    Private _BostonDataType As pcenumORMDataType
+    Public Property BostonDataType As pcenumORMDataType
+        Get
+            Return Me._BostonDataType
+        End Get
+        Set(value As pcenumORMDataType)
+            Me._BostonDataType = value
+        End Set
+    End Property
+
+    Private _DataType As String
+    Public Property DataType As String
+        Get
+            Return Me._DataType
+        End Get
+        Set(value As String)
+            Me._DataType = value
+        End Set
+    End Property
+
+End Class
---
+++ Boston/ClassesFactEngine/tFEQLProcessorORMFunctions.vb
@@ -0,0 +1,27 @@
+﻿Namespace FEQL
+    Partial Public Class Processor
+
+        Public Function doesQueryEdgeHaveAnRingConstraint(ByRef arQueryEdge As FactEngine.QueryEdge) As Boolean
+
+            Try
+                If arQueryEdge.BaseNode.FBMModelObject IsNot arQueryEdge.TargetNode.FBMModelObject Then
+                    Return False
+                Else
+                    For Each lrRingConstraint In Me.Model.RoleConstraint.FindAll(Function(x) x.RoleConstraintType = pcenumRoleConstraintType.RingConstraint)
+                        If lrRingConstraint.Role(0).JoinedORMObject Is arQueryEdge.BaseNode.FBMModelObject Then
+                            Return True
+                        End If
+                    Next
+                End If
+
+                Return False
+
+            Catch ex As Exception
+                Debugger.Break()
+                Return False
+            End Try
+
+        End Function
+
+    End Class
+End Namespace
