ba4127f7d48cbe8536a87abd442bff25538b5abd
Viev <victormorgante@viev.com>
2021-05-16 09:23:57 +1000

    - Minor fix to loading Roles for a RoleConstraint. Now won't add RoleConstraintRole if Role does not exist in Model.

--- Boston/My Project/Settings.Designer.vb
+++ Boston/My Project/Settings.Designer.vb
@@ -320,7 +320,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
         Public Property DisplayCoreMetaModel() As Boolean
             Get
                 Return CType(Me("DisplayCoreMetaModel"),Boolean)
