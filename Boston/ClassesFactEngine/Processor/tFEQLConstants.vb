7572495f16feee785357d09fc0e4a7a20ac6df8a
Viev <victormorgante@viev.com>
2021-08-11 14:50:54 +1000

    - Implemented tFEQLConstants.vb and pcenumFEQLComparitor for comparitors in FEQL. Checking in.

---
+++ Boston/ClassesFactEngine/Processor/tFEQLConstants.vb
@@ -0,0 +1,11 @@
+﻿Namespace FEQL
+    Public Module tFEQLConstants
+
+        Public Enum pcenumFEQLComparitor
+            Bang
+            Carret
+            Colon
+        End Enum
+
+    End Module
+End Namespace
