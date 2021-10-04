61b17f372579b4276f9aea001815b38dbf4d4571
Viev <victormorgante@viev.com>
2021-10-04 12:43:04 +1000

    - Improving model Fix regime.

---
+++ Boston/Forms/DialogBoxes/frmFixModelErrors.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFixModelErrors
+    Inherits WeifenLuo.WinFormsUI.Docking.DockContent
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.LabelPromptWhatToDo = New System.Windows.Forms.Label()
+        Me.ButtonFixModelErrors = New System.Windows.Forms.Button()
+        Me.CheckedListBoxFixTypes = New System.Windows.Forms.CheckedListBox()
+        Me.LabelPromptModel = New System.Windows.Forms.Label()
+        Me.LabelModelName = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.LabelModelName)
+        Me.GroupBox1.Controls.Add(Me.LabelPromptModel)
+        Me.GroupBox1.Controls.Add(Me.LabelPromptWhatToDo)
+        Me.GroupBox1.Controls.Add(Me.ButtonFixModelErrors)
+        Me.GroupBox1.Controls.Add(Me.CheckedListBoxFixTypes)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(962, 486)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'LabelPromptWhatToDo
+        '
+        Me.LabelPromptWhatToDo.AutoSize = True
+        Me.LabelPromptWhatToDo.Location = New System.Drawing.Point(117, 46)
+        Me.LabelPromptWhatToDo.Name = "LabelPromptWhatToDo"
+        Me.LabelPromptWhatToDo.Size = New System.Drawing.Size(238, 13)
+        Me.LabelPromptWhatToDo.TabIndex = 2
+        Me.LabelPromptWhatToDo.Text = "Check which type of model errors you want fixed."
+        '
+        'ButtonFixModelErrors
+        '
+        Me.ButtonFixModelErrors.Location = New System.Drawing.Point(6, 41)
+        Me.ButtonFixModelErrors.Name = "ButtonFixModelErrors"
+        Me.ButtonFixModelErrors.Size = New System.Drawing.Size(105, 23)
+        Me.ButtonFixModelErrors.TabIndex = 1
+        Me.ButtonFixModelErrors.Text = "&Fix Model Errors"
+        Me.ButtonFixModelErrors.UseVisualStyleBackColor = True
+        '
+        'CheckedListBoxFixTypes
+        '
+        Me.CheckedListBoxFixTypes.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CheckedListBoxFixTypes.FormattingEnabled = True
+        Me.CheckedListBoxFixTypes.Items.AddRange(New Object() {"Roles without JoinedORMObject. Remove from Model.", "Relations, Invalid ActiveRole on OriginColumns (OriginColumn.ActiveRole <> Destin" &
+                "ation.ActiveRole). OriginColumn.Count <> DestinationColumn.Count", "Columns where ActiveRole is Nothing. Try and fix.", "Columns where ActiveRole is Nothing. Remove the Column.", "InternalUniquenessConstraints, where LevelNumbers are not correct. Correct the le" &
+                "vel numbers.", "Column Ordinal Positions. Reset where out of synchronous order.", "RDS Tables with no Columns. Remove those Tables.", "RDS Columns that should be Mandatory (i.e. on Objectifed Fact Types). Make mandat" &
+                "ory.", "Remove FactTypeInstances from Page where FactTypeIntance has RoleInstance that Jo" &
+                "ins nothing.", "Relational Data Structure, Tables where the number of PrimaryKey columns does not" &
+                " match the number of Roles in the PreferredIdentifier. Fix that.", "Duplicate Facts. Remove duplicates.", "RDS Tables and PGS Nodes (Nodes/Tables of ObjectifiedFactTypes) that are missing " &
+                "Relations. Add the relations.", "RDS Tables with more than one relation for the same FactType/Join. Prune extra re" &
+                "lations.", "RDS Relations, that have no OriginColumns. Try and create the OriginColumns."})
+        Me.CheckedListBoxFixTypes.Location = New System.Drawing.Point(6, 70)
+        Me.CheckedListBoxFixTypes.Name = "CheckedListBoxFixTypes"
+        Me.CheckedListBoxFixTypes.Size = New System.Drawing.Size(950, 409)
+        Me.CheckedListBoxFixTypes.TabIndex = 0
+        '
+        'LabelPromptModel
+        '
+        Me.LabelPromptModel.AutoSize = True
+        Me.LabelPromptModel.Location = New System.Drawing.Point(6, 16)
+        Me.LabelPromptModel.Name = "LabelPromptModel"
+        Me.LabelPromptModel.Size = New System.Drawing.Size(39, 13)
+        Me.LabelPromptModel.TabIndex = 3
+        Me.LabelPromptModel.Text = "Model:"
+        '
+        'LabelModelName
+        '
+        Me.LabelModelName.AutoSize = True
+        Me.LabelModelName.Location = New System.Drawing.Point(51, 16)
+        Me.LabelModelName.Name = "LabelModelName"
+        Me.LabelModelName.Size = New System.Drawing.Size(90, 13)
+        Me.LabelModelName.TabIndex = 4
+        Me.LabelModelName.Text = "LabelModelName"
+        '
+        'frmFixModelErrors
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(986, 510)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmFixModelErrors"
+        Me.Text = "Fix Model Errors"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents LabelPromptWhatToDo As Label
+    Friend WithEvents ButtonFixModelErrors As Button
+    Friend WithEvents CheckedListBoxFixTypes As CheckedListBox
+    Friend WithEvents LabelModelName As Label
+    Friend WithEvents LabelPromptModel As Label
+End Class
---
+++ Boston/Forms/DialogBoxes/frmFixModelErrors.vb
@@ -0,0 +1,115 @@
+﻿Imports System.Reflection
+
+Public Class frmFixModelErrors
+
+    Public mrModel As FBM.Model
+
+    Private Sub frmFixModelErrors_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Call Me.SetupForm()
+
+    End Sub
+
+    Public Sub SetupForm()
+
+        Try
+
+            Me.LabelModelName.Text = Me.mrModel.Name
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RolesWithoutJoinedORMObject,
+                                                                  "Roles Without JoinedORMObject. Remove from Model.",
+                                                                  pcenumModelFixType.RolesWithoutJoinedORMObject))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RelationsInvalidActiveRoleOnOriginColumns,
+                                                                  "RDS Relations. Invalid ActiveRole On OriginColumns.",
+                                                                  pcenumModelFixType.RelationsInvalidActiveRoleOnOriginColumns))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.ColumnsWhereActiveRoleIsNothingTryAndFix,
+                                                                  "RDS Columns Where ActiveRole Is Nothing. TryAndFix.",
+                                                                  pcenumModelFixType.ColumnsWhereActiveRoleIsNothingTryAndFix))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.ColumnsWhereActiveRoleIsNothingRemoveTheColumn,
+                                                                  "RDS Columns Where ActiveRole Is Nothing. Remove The Column.",
+                                                                  pcenumModelFixType.ColumnsWhereActiveRoleIsNothingRemoveTheColumn))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.InternalUniquenessConstraintsWhereLevelNumbersAreNotCorrect,
+                                                                  "Internal Uniqueness Constraints Where Level Numbers Are Not Correct. Fix.",
+                                                                  pcenumModelFixType.InternalUniquenessConstraintsWhereLevelNumbersAreNotCorrect))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.ColumnOrdinalPositionsResetWhereOutOfSynchronousOrder,
+                                                                  "RDS Column. Ordinal Positions. Reset Where Out Of Synchronous Order.",
+                                                                  pcenumModelFixType.ColumnOrdinalPositionsResetWhereOutOfSynchronousOrder))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RDSTablesWithNoColumnsRemoveThoseTables,
+                                                                  "RDS Tables With No Columns. Remove Those Tables.",
+                                                                  pcenumModelFixType.RDSTablesWithNoColumnsRemoveThoseTables))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RDSColumnsThatShouldBeMandatoryMakeMandatory,
+                                                                  "RDS Columns That Should Be Mandatory. Make Mandatory.",
+                                                                  pcenumModelFixType.RDSColumnsThatShouldBeMandatoryMakeMandatory))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RemoveFactTypeInstancesFromPageWhereFactTypeIntanceHasRoleInstanceThatJoinsNothing,
+                                                                  "Remove FactTypeInstances From Page Where FactTypeIntance Has RoleInstance That JoinsNothing.",
+                                                                  pcenumModelFixType.RemoveFactTypeInstancesFromPageWhereFactTypeIntanceHasRoleInstanceThatJoinsNothing))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RDSTablesWhereTheNumberOfPrimaryKeyColumnsDoesNotMatchTheNumberOfRolesInThePreferredIdentifierFixThat,
+                                                                  "RDS Tables Where The Number Of PrimaryKey Columns Does Not Match The Number Of Roles In The PreferredIdentifier. FixThat.",
+                                                                  pcenumModelFixType.RDSTablesWhereTheNumberOfPrimaryKeyColumnsDoesNotMatchTheNumberOfRolesInThePreferredIdentifierFixThat))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.DuplicateFactsRemoveDuplicates,
+                                                                  "Duplicate Facts. Remove Duplicates.",
+                                                                  pcenumModelFixType.DuplicateFactsRemoveDuplicates))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RDSTablesAndPGSNodesThatAreMissingRelationsAddTheRelations,
+                                                                  "RDS Tables And PGS Nodes That Are Missing Relations. Add The Relations.",
+                                                                  pcenumModelFixType.RDSTablesAndPGSNodesThatAreMissingRelationsAddTheRelations))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RDSTablesWithMoreThanOneRelationForTheSameFactTypeJoinPruneExtraRelations,
+                                                                  "RDS Tables With More Than One Relation For The Same Fact Type Join. Prune Extra Relations.",
+                                                                  pcenumModelFixType.RDSTablesWithMoreThanOneRelationForTheSameFactTypeJoinPruneExtraRelations))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RDSRelationsThatHaveNoOriginColumnsRemoveRelation,
+                                                                  "RDS Relations That Have No Origin Columns. Remove Relation.",
+                                                                  pcenumModelFixType.RDSRelationsThatHaveNoOriginColumnsRemoveRelation))
+
+            Me.CheckedListBoxFixTypes.Items.Add(New tComboboxItem(pcenumModelFixType.RDSRelationsThatHaveOriginTableButNoDestinationTableAndViceVersa,
+                                                                  "RDS Relations that have an origin table but no destination table, and vice versa. Remove from Model.",
+                                                                  pcenumModelFixType.RDSRelationsThatHaveOriginTableButNoDestinationTableAndViceVersa))
+
+        Catch ex As Exception
+            Dim lsMessage As String
+            Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+            lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+            lsMessage &= vbCrLf & vbCrLf & ex.Message
+            prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Critical, ex.StackTrace)
+        End Try
+
+    End Sub
+
+    Private Sub ButtonFixModelErrors_Click(sender As Object, e As EventArgs) Handles ButtonFixModelErrors.Click
+
+        Try
+            If Me.CheckedListBoxFixTypes.CheckedItems.Count = 0 Then
+                MsgBox("Select at least one type of model fix that you would like to execute.")
+                Exit Sub
+            End If
+
+            If MsgBox("Have you backed up the Boston database?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+
+
+
+            End If
+
+
+        Catch ex As Exception
+            Dim lsMessage As String
+            Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+            lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+            lsMessage &= vbCrLf & vbCrLf & ex.Message
+            prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Critical, ex.StackTrace)
+        End Try
+
+    End Sub
+End Class
