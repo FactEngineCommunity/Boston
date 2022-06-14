b838585f884b1b684319641f62d3836afd951037
Viev <victormorgante@viev.com>
2022-06-14 12:34:15 +1000

    - Getting ready to implement Use Case Diagrams and BPMN.

---
+++ Boston/ClassesLANGUAGEORM/ORMUCD/tORMModel.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Reflection
+
+Namespace FBM
+
+    Partial Public Class Model
+
+        Public Function createCMMLProcess(ByVal asProcessText As String) As CMML.Process
+
+            Dim lrProcess = New CMML.Process
+            lrProcess.Text = asProcessText
+            lrProcess.Model = Me
+
+            'Me.CMML.Process.AddUnique(lrProcess)
+
+            Dim lsSQLQuery As String
+
+            lsSQLQuery = "INSERT INTO " & pcenumCMMLRelations.CoreElementHasElementType.ToString & " (Element, ElementType)"
+            lsSQLQuery &= " VALUES ('" & lrProcess.Id & "','Process'"
+            lsSQLQuery &= ")"
+
+            Dim lrFact As FBM.Fact = Me.ORMQL.ProcessORMQLStatement(lsSQLQuery)
+
+            lsSQLQuery = "INSERT INTO " & pcenumCMMLRelations.CoreProcessHasProcessText.ToString & " (Process, ProcessText)"
+            lsSQLQuery &= " VALUES ('" & lrProcess.Id & "','" & lrProcess.Text & "'"
+            lsSQLQuery &= ")"
+
+            Me.ORMQL.ProcessORMQLStatement(lsSQLQuery)
+
+            Return lrProcess
+
+        End Function
+
+        Public Sub updateCMMLProcessText(ByRef arProcess As CMML.Process)
+
+            Try
+                Dim lsSQLQuery As String
+
+                lsSQLQuery = "UPDATE " & pcenumCMMLRelations.CoreProcessHasProcessText.ToString
+                lsSQLQuery &= " SET ProcessText = '" & arProcess.Text & "'"
+                lsSQLQuery &= " WHERE Process = '" & arProcess.Id & "'"
+
+                Me.ORMQL.ProcessORMQLStatement(lsSQLQuery)
+
+            Catch ex As Exception
+                Dim lsMessage As String
+                Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+                lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+                lsMessage &= vbCrLf & vbCrLf & ex.Message
+                prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Critical, ex.StackTrace)
+            End Try
+
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ Boston/ClassesLANGUAGEORM/ORMUCD/tORMPage.vb
@@ -0,0 +1,77 @@
+﻿Imports Boston.FBM.STM
+Imports System.Reflection
+Imports Newtonsoft.Json
+
+Namespace FBM
+
+    Partial Public Class Page
+        <JsonIgnore()>
+        <NonSerialized>
+        Public WithEvents CMMLModel As CMML.Model
+
+        ''' <summary>
+        ''' 
+        ''' </summary>
+        ''' <param name="arProcess">The Model Level Process</param>
+        ''' <param name="aoPointF"></param>
+        ''' <param name="aoContainerNode"></param>
+        ''' <returns></returns>
+        Public Function DropProcessAtPoint(ByRef arProcess As CMML.Process,
+                                           ByVal aoPointF As PointF,
+                                           ByRef aoContainerNode As MindFusion.Diagramming.ContainerNode,
+                                           Optional ByVal abBroadcastInterfaceEvent As Boolean = True) As CMML.Process
+
+            Dim lsSQLQuery As String = ""
+            Dim lrFactInstance As FBM.FactInstance
+            Dim lrRecordset As ORMQL.Recordset
+            Dim lsMessage As String = ""
+
+            Try
+                lsSQLQuery = "SELECT *"
+                lsSQLQuery &= " FROM " & pcenumCMMLRelations.CoreElementHasElementType.ToString
+                lsSQLQuery &= " WHERE Element = '" & arProcess.Id & "'"
+
+                lrRecordset = Me.Model.ORMQL.ProcessORMQLStatement(lsSQLQuery)
+
+                If lrRecordset.EOF Then
+                    lsMessage = "The Process, '" & arProcess.Text & "', does not seem to exist at the Model level."
+
+                    Call prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Information, Nothing, False, False, True)
+                    Return Nothing
+                Else
+
+                    lsSQLQuery = "ADD FACT '" & lrRecordset.CurrentFact.Id & "'"
+                    lsSQLQuery &= " TO " & pcenumCMMLRelations.CoreElementHasElementType.ToString
+                    lsSQLQuery &= " ON PAGE '" & Me.Name & "'"
+
+                    lrFactInstance = Me.Model.ORMQL.ProcessORMQLStatement(lsSQLQuery)
+
+                    Dim lrProcess As CMML.Process = lrFactInstance.GetFactDataInstanceByRoleName(pcenumCMML.Element.ToString).CloneProcess(Me)
+                    lrProcess.Text = arProcess.Text
+                    Me.UseCaseDiagram.Process.AddUnique(lrProcess)
+                    '===================================================================================================================
+
+                    Call lrProcess.Move(aoPointF.X, aoPointF.Y, abBroadcastInterfaceEvent)
+
+                    Call Me.Save(False, False)
+
+                    Call lrProcess.DisplayAndAssociate(aoContainerNode)
+
+                    Return lrProcess
+                End If
+
+            Catch ex As Exception
+                Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+                lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+                lsMessage &= vbCrLf & vbCrLf & ex.Message
+                prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Critical, ex.StackTrace)
+
+                Return Nothing
+            End Try
+
+        End Function
+
+    End Class
+
+End Namespace
