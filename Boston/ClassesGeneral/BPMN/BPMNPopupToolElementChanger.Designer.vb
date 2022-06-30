2be6675c8e7967bb7a3704b0a8938bb9f1ff736b
Viev <victormorgante@viev.com>
2022-06-30 21:35:39 +1000

    - Getting ready for BPMN

---
+++ Boston/ClassesGeneral/BPMN/BPMNPopupToolElementChanger.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class BPMNPopupToolElementChanger
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.LabelPromptChangeElement = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox = New ExtListAndCombo.ExtListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LabelPromptChangeElement
+        '
+        Me.LabelPromptChangeElement.AutoSize = True
+        Me.LabelPromptChangeElement.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelPromptChangeElement.ForeColor = System.Drawing.Color.DimGray
+        Me.LabelPromptChangeElement.Location = New System.Drawing.Point(3, 9)
+        Me.LabelPromptChangeElement.Name = "LabelPromptChangeElement"
+        Me.LabelPromptChangeElement.Size = New System.Drawing.Size(133, 19)
+        Me.LabelPromptChangeElement.TabIndex = 0
+        Me.LabelPromptChangeElement.Text = "Change element"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(33, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(231, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ListBox
+        '
+        Me.ListBox.AlignText = True
+        Me.ListBox.CaretIndex = -1
+        Me.ListBox.DefaultImage = Nothing
+        Me.ListBox.DisplayMode = CType((ExtListAndCombo.DisplayMode.ShowText Or ExtListAndCombo.DisplayMode.ShowImage), ExtListAndCombo.DisplayMode)
+        Me.ListBox.DrawMode = ExtListAndCombo.DrawMode.UseFixedHeight
+        Me.ListBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox.FormattingEnabled = True
+        Me.ListBox.HighlightBackColor = System.Drawing.SystemColors.Highlight
+        Me.ListBox.HighlightTextColor = System.Drawing.SystemColors.HighlightText
+        Me.ListBox.ImagePadding = New System.Windows.Forms.Padding(1)
+        Me.ListBox.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ListBox.ItemHeight = 18
+        Me.ListBox.Location = New System.Drawing.Point(7, 79)
+        Me.ListBox.Name = "ListBox"
+        Me.ListBox.SelectedItemInfo = Nothing
+        Me.ListBox.Size = New System.Drawing.Size(257, 220)
+        Me.ListBox.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Boston.My.Resources.Resources.Spyglass16x16
+        Me.PictureBox1.Location = New System.Drawing.Point(7, 42)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(20, 20)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'BPMNPopupToolElementChanger
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Controls.Add(Me.ListBox)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.LabelPromptChangeElement)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.Name = "BPMNPopupToolElementChanger"
+        Me.Size = New System.Drawing.Size(281, 326)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelPromptChangeElement As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ListBox As ExtListAndCombo.ExtListBox
+End Class
---
+++ Boston/ClassesGeneral/BPMN/BPMNPopupToolElementChanger.vb
@@ -0,0 +1,162 @@
+﻿Imports System.Reflection
+
+Public Class BPMNPopupToolElementChanger
+
+    Public Type As pcenumBPMNElementType
+    Public Result As String
+
+    Public mrCMMLModel As CMML.Model
+    Public msLinkedProcessId As String
+    Public moDiagram As MindFusion.Diagramming.Diagram
+
+    Public mrPage As FBM.Page
+    Public Node As MindFusion.Diagramming.WinForms.ControlNode
+
+    Private Delegate Sub MyDelegate(ByRef arPage As FBM.Page, ByRef asInstructionType As String)
+
+    Private Sub BPMNPopupToolSelector_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Call Me.SetupForm()
+
+    End Sub
+
+    Public Sub SetupForm(Optional asFilter As String = Nothing)
+
+        Try
+            Select Case Me.Type
+                Case Is = pcenumBPMNElementType.Activity
+                    Call Me.SetupForBPMNActivity(asFilter)
+            End Select
+
+        Catch ex As Exception
+            Dim lsMessage As String
+            Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+            lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+            lsMessage &= vbCrLf & vbCrLf & ex.Message
+            prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Critical, ex.StackTrace)
+        End Try
+    End Sub
+
+    Private Sub SetupForBPMNActivity(Optional asFilter As String = Nothing)
+
+        Try
+            Dim lasList() As String = {"Task",
+                                       "Send Task",
+                                       "Receive Task",
+                                       "User Task",
+                                       "Manual Task",
+                                       "Business Rule Task",
+                                       "Service Task",
+                                       "Script Task",
+                                       "Call Activity"
+                                        }
+
+            Me.ListBox.Items.Clear()
+
+            With Me.ListBox
+                'Fill the list-box
+                .UseCustomTabOffsets = True
+
+                With .Items
+                    For Each lsListItem In lasList
+                        If asFilter IsNot Nothing Then
+                            If lsListItem.IndexOf(asFilter, StringComparison.CurrentCultureIgnoreCase) = -1 Then
+                                GoTo SkipItem
+                            End If
+                        End If
+                        Select Case lsListItem
+                            Case Is = "Task"
+                                .Add("Task", My.Resources.BPMNElementDecorations.Task_25x25,,,)
+                            Case Is = "Send Task"
+                                .Add("Send Task", My.Resources.BPMNElementDecorations.TaskType_Send,,,)
+                            Case Is = "Receive Task"
+                                .Add("Receive Task", My.Resources.BPMNElementDecorations.TaskType_Receive,,,)
+                            Case Is = "User Task"
+                                .Add("User Task", My.Resources.BPMNElementDecorations.TaskType_User,,,)
+                            Case Is = "Manual Task"
+                                .Add("Manual Task", My.Resources.BPMNElementDecorations.TaskType_Manual,,,)
+                            Case Is = "Business Rule Task"
+                                .Add("Business Rule Task", My.Resources.BPMNElementDecorations.BusinessRule_25x25,,,)
+                            Case Is = "Service Task"
+                                .Add("Service Task", My.Resources.BPMNElementDecorations.TaskType_Service,,,)
+                            Case Is = "Script Task"
+                                .Add("Script Task", My.Resources.BPMNElementDecorations.TaskType_Script,,,)
+                            Case Is = "Call Activity"
+                                .Add("Call Activity", My.Resources.BPMNElementDecorations.CallActivity_25x25,,,)
+                        End Select
+SkipItem:
+                    Next
+
+
+
+                End With
+
+            End With
+
+        Catch ex As Exception
+            Dim lsMessage As String
+            Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+            lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+            lsMessage &= vbCrLf & vbCrLf & ex.Message
+            prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Critical, ex.StackTrace)
+        End Try
+
+    End Sub
+
+    Private Sub ListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox.SelectedIndexChanged
+
+        Try
+            Select Case Me.ListBox.SelectedItem
+                Case Is = "Send Task"
+                    Me.Result = "ChangeElementActivityTaskTypeSendTask"
+                Case Is = "Receive Task"
+                    Me.Result = "ChangeElementActivityTaskTypeReceiveTask"
+                Case Is = "Manual Task"
+                    Me.Result = "ChangeElementActivityTaskTypeManualTask"
+                Case Is = "Business Rule Task"
+                    Me.Result = "ChangeElementActivityTaskTypeBusinessRuleTask"
+                Case Is = "Service Task"
+                    Me.Result = "ChangeElementActivityTaskTypeServiceTask"
+                Case Is = "Script Task"
+                    Me.Result = "ChangeElementActivityTaskTypeScriptTask"
+                Case Is = "User Task"
+                    Me.Result = "ChangeElementActivityTaskTypeUserTask"
+            End Select
+
+            Me.moDiagram.Nodes.Remove(Me.Node)
+
+            Dim delegate1 As MyDelegate = New MyDelegate(AddressOf frmDiagramBPMNCollaboration.ProcessPopupToolSelector)
+            delegate1(Me.mrPage, Me.Result)
+
+        Catch ex As Exception
+            Dim lsMessage As String
+            Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+            lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+            lsMessage &= vbCrLf & vbCrLf & ex.Message
+            prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Critical, ex.StackTrace)
+        End Try
+
+    End Sub
+
+    Private Sub TextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyUp
+
+        Try
+            If Trim(Me.TextBox1.Text).Length > 0 Then
+                Call Me.SetupForm(Trim(Me.TextBox1.Text))
+            Else
+                Call SetupForm()
+            End If
+        Catch ex As Exception
+            Dim lsMessage As String
+            Dim mb As MethodBase = MethodInfo.GetCurrentMethod()
+
+            lsMessage = "Error: " & mb.ReflectedType.Name & "." & mb.Name
+            lsMessage &= vbCrLf & vbCrLf & ex.Message
+            prApplication.ThrowErrorMessage(lsMessage, pcenumErrorType.Critical, ex.StackTrace)
+        End Try
+
+    End Sub
+End Class
