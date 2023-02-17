3901879d33bc3a93649317ebe2c778a642ec4e2a
Viev <victormorgante@viev.com>
2023-02-18 07:45:47 +1000

    - FEQL parsing. Introduced QueryFormulaToken. QueryNodes can be formula under certain circumnstances;

---
+++ Boston/ClassesFactEngine/Parsing/QueryGraph/Formula/tQueryFormulaToken.vb
@@ -0,0 +1,39 @@
+﻿Namespace FactEngine
+
+    ''' <summary>
+    ''' A QueryEdge may have a Formula, which is a List(Of tQueryFormulaToken).
+    '''   NB The classes QueryFormulaToken and QueryNode both inherit from tQueryFormulaToken.
+    ''' </summary>
+    Public MustInherit Class tQueryFormulaToken
+    End Class
+
+    Public Class QueryFormulaToken
+        Inherits tQueryFormulaToken
+
+        ''' <summary>
+        ''' A Token can be +,-,*,/,[Number],[ReservedWord],(,),=,<,>,<=,>=
+        ''' NB A QueryNode can also be a QueryFormulaToken and inherits tQueryFormulaToken
+        ''' </summary>
+        Private _TOKEN As String = Nothing
+        Public Property TOKEN As String
+            Get
+                Return Me._TOKEN
+            End Get
+            Set(value As String)
+                Me._TOKEN = value
+            End Set
+        End Property
+
+        ''' <summary>
+        ''' Parameterless Constructor
+        ''' </summary>
+        Public Sub New()
+        End Sub
+
+        Public Sub New(ByVal asToken As String)
+            Me.TOKEN = asToken
+        End Sub
+
+    End Class
+
+End Namespace
